414cb62da3f0d3ebcc77c146aa51a5f1a1202b72
Shritesh Bhattarai <shr@ite.sh>
2020-05-21 11:29:30 -0500

    Implement Add and Mul for Vec2

--- src/linarg.rs
+++ src/linarg.rs
@@ -1,4 +1,6 @@
 use js_sys::Float32Array;
+use std::ops::{Add, Mul};
+#[derive(Copy, Clone)]
 pub struct Vec2(pub f32, pub f32);
 
 impl Vec2 {
@@ -11,5 +13,19 @@ impl Vec2 {
         }
 
         f32array
+    }
+}
+
+impl Add for Vec2 {
+    type Output = Vec2;
+    fn add(self, rhs: Self) -> Self::Output {
+        Vec2(self.0 + rhs.0, self.1 + rhs.1)
+    }
+}
+
+impl Mul<f32> for Vec2 {
+    type Output = Vec2;
+    fn mul(self, rhs: f32) -> Self::Output {
+        Vec2(self.0 * rhs, self.1 * rhs)
     }
 }
